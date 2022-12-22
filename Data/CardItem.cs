33f1e0808a11dd5ada2cb505baaf9f79883dbebb
Keanu Barnard <keanu.barnardd@gmail.com>
2022-12-23 07:16:04 +1100

    Card index added to cards

--- Data/CardItem.cs
+++ Data/CardItem.cs
@@ -2,6 +2,7 @@ namespace card_storm
 {
   public class CardItem
   {
+    public int cardNumber { get; set; }
     public string? question { get; set; }
     public string? answer { get; set; }
   }
