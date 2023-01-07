aa2d1bc87ad84ae5374f4cb8fce2f5e81aa8371c
Keanu Barnard <keanu.barnardd@gmail.com>
2023-01-07 21:20:13 +1100

    Database set up

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using card_storm.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddSingleton<StoreService>();
 
 var app = builder.Build();
 
