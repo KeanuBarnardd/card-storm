d80f943531c24caa29372d658ab3a121b0b9b243
Keanu Barnard <keanu.barnardd@gmail.com>
2023-01-10 19:56:41 +1100

    merge

---
+++ Data/CartItem.cs
@@ -0,0 +1,10 @@
+namespace card_storm.Data
+{
+  public class CartItem
+  {
+    public int id { get; set; }
+    public string title { get; set; }
+    public string price { get; set; }
+    public int count { get; set; }
+  }
+}
